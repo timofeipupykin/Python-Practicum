c137618674cb702302f10f4936283817208bef00
timofeipupykin <pupykint@gmail.com>
2024-11-29 16:06:50 +0300

    что-то

--- 2.5/2.5.1.py
+++ 2.5/2.5.1.py
@@ -1,5 +1,5 @@
 def main():
-    a, b, c = [int(input()) for i in range(3)]
+    a, b, c = [int(input()) for _ in range(3)]
     r = (a ** b) % (a + c)
     print(f"({a} ^ {b}) mod ({a} + {c}) = {r}")
 if __name__ == "__main__":
